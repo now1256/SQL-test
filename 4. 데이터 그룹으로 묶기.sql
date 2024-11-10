e93f43b9934e8176a76049651175befbc9b03430
jaeheon <now1256@naver.com>
2024-11-10 14:06:02 +0900

    Update and rename 4. 데이터 그룹으로 묶기 to 4. 데이터 그룹으로 묶기.sql

--- 4. 데이터 그룹으로 묶기.sql
+++ 4. 데이터 그룹으로 묶기.sql
@@ -1,10 +1,13 @@
 # 집계함수 AVG , variance 사용 
 # ROUND를 통해 몇자리까지 올릴지 결정 -> ROUND말고도 많아서 정리해야함 
 # quarter의 열의 값을 기준으로 그룹핑해서 열정리 
-SELECT quartet,
-Round(AVG(x),2) AS x_mean,
-Round(variance(x),2) AS x_var,
-Round(AVG(y), 2) AS y_mean,
-Round(variance(y),2) AS y_var 
-FROM points
-GROUP BY quartet
+SELECT
+  quartet,
+  Round(AVG(x), 2) AS x_mean,
+  Round(variance(x), 2) AS x_var,
+  Round(AVG(y), 2) AS y_mean,
+  Round(variance(y), 2) AS y_var
+FROM
+  points
+GROUP BY
+  quartet
