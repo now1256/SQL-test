e4ba43a7b96e6bc298d184629d59ea4cf959cf56
jaeheon <now1256@naver.com>
2024-11-10 14:06:23 +0900

    Update 4. 데이터 그룹으로 묶기.sql

--- 4. 데이터 그룹으로 묶기.sql
+++ 4. 데이터 그룹으로 묶기.sql
@@ -1,6 +1,6 @@
 # 집계함수 AVG , variance 사용 
 # ROUND를 통해 몇자리까지 올릴지 결정 -> ROUND말고도 많아서 정리해야함 
-# quarter의 열의 값을 기준으로 그룹핑해서 열정리 
+# quarter의 열의 값을 기준으로 그룹핑해서 열 정리 
 SELECT
   quartet,
   Round(AVG(x), 2) AS x_mean,
